902261541a75583d6ac36796ed0f7282c5749b5e
kwamkid <kwamkid@gmail.com>
2025-08-02 20:25:16 +0700

    Add vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
-// vite.config.ts ต้องมีส่วนนี้
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // เพิ่มบรรทัดนี้
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: 'dist', // ตรวจสอบให้แน่ใจว่า build ไปที่ dist
+    sourcemap: false,
+    chunkSizeWarningLimit: 1600,
+  }
 })
